8f3c3937be52126217f4df3961bafa6b825245f5
infinitykingz <absolute274@gmail.com>
2018-08-13 13:58:34 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'W9mSbH8dLHWGD65wwrXW76c+/XgpT8OlOCtkCIfvaEVDr7WZ1Z3j2Vq9Oa9fq6SKKe6oQtKAUrzLieiWwESIe/Ad2MbxXTyrVHyAHwl2X2AGAfZ8tDv72avSj7potdhgPQaNhj1ctb53MxtUf6kU+QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
